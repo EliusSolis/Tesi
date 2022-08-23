5e6e565bf0f29c7c27fdbc15a677016293cd50a1
Elius <elios2405@gmail.com>
2022-08-23 21:46:37 +0200

    update generate.js

--- Tosca-Siem-test/js/test.js
+++ Tosca-Siem-test/js/test.js
@@ -7,6 +7,8 @@ const tosca = require('tosca.lib.utils');
 traversal.coerce();
 
 let tf = '';
+let images = []
+let volumes = []
 writeProvider();
 
 // ciclo for per ogni nodo
@@ -16,11 +18,29 @@ for (let id in clout.vertexes) {
         tf += convert(vertex);
     }
 }
-
+setupImages();
+setupVolumes();
 puccini.write(tf);
 
 function writeProvider(){   // scrive prime righe necessarie per tf
     tf += 'terraform {\nrequired_providers {\ndocker = {\nsource  = "kreuzwerker/docker"\nversion = "~> 2.13.0"\n}\n}\n}\n\nprovider "docker" {}\n\n';
+}
+
+function setupImages(){
+    for (let i = 0; i < images.length; i++) {
+        tf += 'resource "docker_image" "image'+i+'" {\n';
+        tf += 'name = "'+images[i]+'"\n';
+        tf += '}\n';
+    }
+
+}
+
+function setupVolumes(){ // todo
+    for (let volume in volumes){
+        tf += 'volume '+volume+'\n';
+
+    }
+
 }
 
 
@@ -35,46 +55,123 @@ function convert(node){  // dato un nodo tosca ritorna la stringa tf equivalente
             break;
         }
     }
+    return 'error'
 
     function convertHost(node){
-        return 'Host qui \n';
+        let properties = node.properties;
+        let s= '';
+
+
+        s += 'resource "docker_container" ' + properties['name'] + '{\n';
+        for (let p in properties.properties){
+            let property = properties.properties[p];
+
+            if ( p === 'image'){
+                let name = String(property)
+                if (images.includes[name]){
+                    s += 'image = docker_image.image'+images.indexOf[name]+'.latest\n';
+                }else{
+                    s += 'image = docker_image.image'+images.length+'.latest\n';
+                    images.push(name);
+                }
+                continue;
+            }
+
+
+
+            if(p === 'volumes'){ // TODO
+                let name = String(property)
+                if (images.includes[name]){
+                    s += 'image = docker_image.image'+images.indexOf[name]+'.latest\n';
+                }else{
+                    s += 'image = docker_image.image'+images.length+'.latest\n';
+                    images.push(name);
+                }
+                continue;
+            }
+
+            if(p === 'ports'){
+                s += p + '{\n';  // TODO bisogna cambiare tosca
+                s += 'internal = 9200\n';
+                s += 'external = 9200\n';
+                s += '}\n';
+                s += JSON.stringify(property);
+                continue;
+            }
+
+            if(p === 'env'){
+                s += p + '= [\n';
+                for (let key in property){
+                    s += '"'+key+'='+property[key]+'",\n';
+                }
+                s = s.slice(0, -2);
+                s += '\n]\n';
+                continue;
+            }
+
+
+            s += baseConverter(p, property);
+        }
+
+
+        s += '}\n'
+        return s;
     }
 
     function convertNetwork(node){
-        let properties = node.properties
+        let properties = node.properties;
         let s= '';
         s += 'resource "docker_network" ' + properties['name'] + '{\n';
 
         for (let p in properties.properties){
-            let propertie = properties.properties[p];
-            s += p +' = ';
-            if (typeof propertie === "string"){
-                s+= '"'+propertie+'"';
-            }else{
-                s += propertie;
-            }
-            s += '\n';
+            s += baseConverter(p, properties.properties[p]);
         }
 
         s += 'ipam_config {\n'
         for (let p in properties.attributes){
-
-            let propertie = properties.attributes[p];
-            if (!propertie || p === 'state'){
+            let type = typeof property;
+            if ( p === 'state'){
                 continue;
             }
-            s += p +' = ';
-            if (typeof propertie === "string"){
-                s+= '"'+propertie+'"';
-            }else{
-                s += propertie;
-            }
-            s += '\n';
+            s+= baseConverter(p,properties.attributes[p])
         }
         s += '}\n';
         s += '}\n';
+
         return s;
     }
 
-    return 'error'
+    function convertGeneric(node){
+        let s = ''
+        for (let p in node){
+            s+= baseConverter(p,node[p]);
+        }
+        return s;
+    }
+
+    function baseConverter(name, value){
+        let type = typeof value;
+        let s = '';
+
+        if (!value){
+        return s;
+        }
+
+        switch(type){
+                case "object":
+                    s += name + '{\n';
+                    s += convertGeneric(value);
+                    s += '}\n';
+                    break;
+                case "string":
+                    s += name +' = "'+value+'"';
+                    break;
+                default:
+                    s += name +' = '+value;
+                    break;
+            }
+        s += '\n';
+        return s;
+    }
+
 }
