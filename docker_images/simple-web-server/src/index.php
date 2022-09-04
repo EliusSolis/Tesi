65d68968185d0fb8f9629bc131235b5e0d5e354e
gianzav <gianlucagianluca18@gmail.com>
2022-09-04 16:59:16 +0200

    immagini docker per un client generico con alcune utility e un web server vulnerabile ad http Host header poisoning

---
+++ docker_images/simple-web-server/src/index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <script type="text/javascript" src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/script.js"></script>
+    </head>
+    <body>
+        <h1>Vulnerable web app</h1>
+    </body>
+</html>
